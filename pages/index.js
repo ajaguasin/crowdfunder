87fc7fa615612b5eb14df5a0e7895d1f6a3dbfcd
ajaguasin <ajaguasin@gmail.com>
2019-02-14 23:32:45 -0800

    Install semantic-ui-react
    -Add renderCrowdfunds() method

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
 import React, { Component } from "react";
 import creator from "../ethereum/creator";
 
+import { Card } from "semantic-ui-react";
+
 class CreatorIndex extends Component {
-  async componentDidMount() {
+  static async getInitialProps() {
     const crowdfunds = await creator.methods.getDeployedCrowdfunds().call();
-    console.log(crowdfunds);
+    return { crowdfunds };
   }
 
+  renderCrowdfunds = () => {
+    const items = this.props.crowdfunds.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      };
+    });
+
+    return <Card.Group items={items} />;
+  };
+
   render() {
-    return <div>Home Page</div>;
+    return (
+      <div>
+        <link
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+        />
+        {this.renderCrowdfunds()}
+      </div>
+    );
   }
 }
 
